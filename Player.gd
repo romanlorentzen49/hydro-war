f2c00d9228ff90eedc7bd40a6e9f860585560df0
Roman <47540377+romanlorentzen49@users.noreply.github.com>
2025-04-22 09:40:54 -0700

    Add files via upload

---
+++ Player.gd
@@ -0,0 +1,56 @@
+extends CharacterBody3D
+
+
+var bullet = load("res://bullet.tscn")
+var instance 
+
+@onready var head = $Head
+@onready var camera =  $Head/Camera3D
+@onready var gun_anim = $Head/Camera3D/Gun/AnimationPlayer
+@onready var  gun_barrel = $Head/Camera3D/Gun/RayCast3D
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 10.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 20.0
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * .005)
+		camera.rotate_x(-event.relative.y * .005)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	# Shooting
+	if Input.is_action_pressed("shoot"):
+		if !gun_anim.is_playing():
+			gun_anim.play("Shoot")
+			instance = bullet.instantiate()
+			instance.position = gun_barrel.global_position
+			instance.transform.basis = gun_barrel.global_transform.basis
+			get_parent().add_child(instance)
+	
+	move_and_slide()
